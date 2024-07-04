f611dad8ef5ec2265a8ba0e5e24d54495f0cb01d
Krishnaku007 <krishna754107@gmail.com>
2024-07-04 13:46:43 +0530

    Add files via upload

---
+++ 02_scope.js
@@ -0,0 +1,54 @@
+let a = 300;
+
+if (true) {
+  let a = 10;
+  const b = 20;
+  // var c = 30
+//   console.log("Inner:", a);
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// function one() {
+//   const username = "krishna";
+
+//   function two() {
+//     const website = " Youtube";
+//     console.log(username);
+//   }
+
+//   console.log(website);
+//   two()
+// }
+// one()
+
+if (true){
+    const username="Krishna"
+    if(username=="Krishna"){
+        const website="Instagram"
+        // console.log(username+website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+// ***********INterseting example******************
+console.log(addone(6));
+function addone(num) {
+    return num+1
+    
+}
+
+
+
+    console.log(addTwo(7)); //we dont use beforre declare in this method
+
+    const addTwo/*expression*/=function(num){
+        return num+2
+ 
+   }
+
+   
+   
