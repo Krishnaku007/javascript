a8afc82b7732c72f78884e5c55bf7a83b4b00c66
krishna kumar <krishna754107@gmail.com>
2024-07-08 07:47:51 +0530

    Add files via upload

---
+++ three.js
@@ -0,0 +1,35 @@
+// for of loop
+
+// ["","",""]
+// [{},{},{}]
+
+// const arr=[1,2,3,4,5]
+// for (const num of arr ) {
+//     console.log(num);
+// }
+
+// const greetigs = "Hello world"
+// for (const greet of greetigs) {
+//     console.log(`Each char is ${greet}`);
+// }
+
+// Maps
+
+// const map=new Map()
+// map.set('IN', "India")
+// map.set('USA', "UNITED STATES OF AMERICA")
+// map.set('UK', "lONDON")
+// // console.log(map);
+
+// for (const [key, Value] of map) {
+//     console.log(key,':-', Value);
+// }
+
+const myObject={
+    'game1':'NFS',
+    'game2':'spiderman'
+
+}
+for (const [key, value] of myObject) {
+    console.log(key,':-', Value);
+}
